26479402b0b0e1325385bc654b35757f465cb6d1
aahan.agrawal <aahan.agrawal@veeva.com>
2024-01-30 20:05:31 +0000

    Add test script

--- starter/starter/test_train_model.py
+++ starter/starter/test_train_model.py
@@ -1,6 +1,48 @@
+import numpy as np
 import pytest
-from starter.starter.ml.model import train_model, compute_model_metrics, inference
+from ml.model import train_model, compute_model_metrics, inference
+from ml.data import process_data
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 
-def test_train_model():
-    pass
+@pytest.fixture
+def setup():
+    data = pd.read_csv("../data/census.csv")
+
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    yield X_train, y_train, encoder, lb
+
+
+def test_train_model(setup):
+    X_train, y_train, _, _ = setup
+    model = train_model(X_train, y_train)
+    assert type(model) == LogisticRegression
+
+
+def test_compute_model_metrics(setup):
+    X_train, y_train, _, _ = setup
+    m1, m2, m3 = compute_model_metrics(y_train, y_train)
+    assert type(m1) == np.float64
+
+
+def test_inference(setup):
+    X_train, y_train, _, _ = setup
+    model = train_model(X_train, y_train)
+    assert type(inference(model, X_train)) == np.ndarray
